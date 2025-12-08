a199b7e892ba5445f2d19ae91ca097c25a255f4e
hp <hp@DESKTOP-D86UIDA>
2025-12-08 06:45:47 -0800

    update UserManagementServiceTest

--- LibraryManagement/src/test/java/librarytest/UserManagementServiceTest.java
+++ LibraryManagement/src/test/java/librarytest/UserManagementServiceTest.java
@@ -2,125 +2,140 @@ package librarytest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import library.service.UserManagementService;
+import library.service.UserUnregisterStatus;
 import libraryy.Admin;
+import libraryy.Book;
 import libraryy.Borrow;
 import libraryy.User;
-import libraryy.Book;
-import java.time.LocalDate;
+import libraryy.MediaItem;
 
 public class UserManagementServiceTest {
 
-   
-
     @Test
     public void testUnregisterUser_AdminNotLoggedIn() {
-        User u = new User("Yumna", "y@mail.com");
-        Admin admin = new Admin("a","1");
+        UserManagementService service = new UserManagementService();
+
+        User user = new User("Yumna", "yumna@mail.com");
+        Admin admin = new Admin("admin", "1234"); 
         List<Borrow> loans = new ArrayList<>();
 
-        UserManagementService s = new UserManagementService();
+        UserUnregisterStatus status = service.unregisterUser(user, admin, loans);
 
-        assertFalse(s.unregisterUser(u, admin, loans));
+        assertEquals(UserUnregisterStatus.ADMIN_NOT_LOGGED_IN, status);
     }
 
     @Test
-    public void testUnregisterUser_UserHasFines() {
-        User u = new User("Yumna", "y@mail.com");
-        u.addFine(10);
+    public void testUnregisterUser_UnpaidFines() {
+        UserManagementService service = new UserManagementService();
 
-        Admin admin = new Admin("a","1");
-        admin.login("a","1");
+        User user = new User("Yumna", "yumna@mail.com");
+        user.addFine(20); 
 
-        UserManagementService s = new UserManagementService();
+        Admin admin = new Admin("admin", "1234");
+        admin.login("admin", "1234"); 
 
-        assertFalse(s.unregisterUser(u, admin, new ArrayList<>()));
+        List<Borrow> loans = new ArrayList<>();
+
+        UserUnregisterStatus status = service.unregisterUser(user, admin, loans);
+
+        assertEquals(UserUnregisterStatus.UNPAID_FINES, status);
     }
 
     @Test
-    public void testUnregisterUser_UserHasActiveLoans() {
-        User u = new User("Yumna", "y@mail.com");
-        Admin admin = new Admin("a","1");
-        admin.login("a","1");
+    public void testUnregisterUser_UserHasActiveBorrowedItem() {
+        UserManagementService service = new UserManagementService();
 
-       
-        Book b = new Book("Java", "A", "111");
-        b.borrow(LocalDate.now());
+        User user = new User("Yumna", "yumna@mail.com");
+
+        Admin admin = new Admin("admin", "1234");
+        admin.login("admin", "1234"); 
 
         List<Borrow> loans = new ArrayList<>();
-        loans.add(new Borrow(b, LocalDate.now(), b.getBorrowPeriod()));
 
-        UserManagementService s = new UserManagementService();
+        
+        Book book = new Book("Java", "Author", "111");
+        book.borrow(LocalDate.now()); 
 
-        assertFalse(s.unregisterUser(u, admin, loans));
+        Borrow borrow = new Borrow(book, LocalDate.now(), book.getBorrowPeriod());
+        loans.add(borrow);
+
+        UserUnregisterStatus status = service.unregisterUser(user, admin, loans);
+
+        assertEquals(UserUnregisterStatus.USER_HAS_ACTIVE_BORROWED_ITEM, status);
     }
 
     @Test
     public void testUnregisterUser_Success() {
-        User u = new User("Yumna", "y@mail.com");
-        Admin admin = new Admin("a","1");
-        admin.login("a","1");
+        UserManagementService service = new UserManagementService();
+
+        User user = new User("Yumna", "yumna@mail.com");
+
+        Admin admin = new Admin("admin", "1234");
+        admin.login("admin", "1234"); 
+        List<Borrow> loans = new ArrayList<>(); 
+
+        UserUnregisterStatus status = service.unregisterUser(user, admin, loans);
+
+        assertEquals(UserUnregisterStatus.USER_UNREGISTERED_SUCCESSFULLY, status);
+    }
+
+    
+
+    @Test
+    public void testUnregister_ThrowsWhenAdminNotLoggedIn() {
+        UserManagementService service = new UserManagementService();
+
+        User user = new User("Yumna", "yumna@mail.com");
+        List<Borrow> loans = new ArrayList<>();
+
+        assertThrows(IllegalStateException.class, () -> {
+            service.unregister(user, loans, false);
+        });
+    }
+
+    @Test
+    public void testUnregister_ThrowsWhenUserHasUnpaidFines() {
+        UserManagementService service = new UserManagementService();
+
+        User user = new User("Yumna", "yumna@mail.com");
+        user.addFine(10); 
+        List<Borrow> loans = new ArrayList<>();
+
+        assertThrows(IllegalStateException.class, () -> {service.unregister(user, loans, true); });
+    }
+
+    @Test
+    public void testUnregister_ThrowsWhenUserHasLoans() {
+        UserManagementService service = new UserManagementService();
+
+        User user = new User("Yumna", "yumna@mail.com");
 
         List<Borrow> loans = new ArrayList<>();
 
-        UserManagementService s = new UserManagementService();
+     
+        MediaItem book = new Book("Java", "Author", "111");
+        Borrow borrow = new Borrow(book, LocalDate.now(), 7);
+        loans.add(borrow);
 
-        assertTrue(s.unregisterUser(u, admin, loans));
-    }
-
-
-   
-    @Test
-    public void testUnregister_ThrowsIfAdminNotLoggedIn() {
-        User u = new User("Yumna", "y@mail.com");
-        UserManagementService s = new UserManagementService();
-
-        assertThrows(IllegalStateException.class, () -> {
-            s.unregister(u, new ArrayList<>(), false);
-        });
-    }
-
-    @Test
-    public void testUnregister_ThrowsIfUserHasFines() {
-        User u = new User("Yumna", "y@mail.com");
-        u.addFine(20);
-
-        UserManagementService s = new UserManagementService();
-
-        assertThrows(IllegalStateException.class, () -> {
-            s.unregister(u, new ArrayList<>(), true);
-        });
-    }
-
-    @Test
-    public void testUnregister_ThrowsIfUserHasLoans() {
-        User u = new User("Yumna", "y@mail.com");
-
-        Book b = new Book("Java", "A", "111");
-        b.borrow(LocalDate.now());
-
-        List<Borrow> loans = new ArrayList<>();
-        loans.add(new Borrow(b, LocalDate.now(), b.getBorrowPeriod()));
-
-        UserManagementService s = new UserManagementService();
-
-        assertThrows(IllegalStateException.class, () -> {
-            s.unregister(u, loans, true);
-        });
+        assertThrows(IllegalStateException.class, () -> {service.unregister(user, loans, true);});
     }
 
     @Test
     public void testUnregister_Success() {
-        User u = new User("Yumna", "y@mail.com");
+        UserManagementService service = new UserManagementService();
+
+        User user = new User("Yumna", "yumna@mail.com");
         List<Borrow> loans = new ArrayList<>();
 
-        UserManagementService s = new UserManagementService();
+        boolean result = service.unregister(user, loans, true); 
 
-        assertTrue(s.unregister(u, loans, true));
+        assertTrue(result);
     }
 }
